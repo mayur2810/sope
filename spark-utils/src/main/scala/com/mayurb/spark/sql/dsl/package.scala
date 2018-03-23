80fd8dd622b56050fa644f8dc7d09f5d5df27675
mayur2810 <mayur.mhi@gmail.com>
2018-03-23 18:20:21 +0530

    refactored, added new methods

--- spark-utils/src/main/scala/com/mayurb/spark/sql/dsl/package.scala
+++ spark-utils/src/main/scala/com/mayurb/spark/sql/dsl/package.scala
@@ -1,7 +1,7 @@
 package com.mayurb.spark.sql
 
 import org.apache.spark.sql.functions.{col, expr}
-import org.apache.spark.sql.{Column, DataFrame, SQLContext}
+import org.apache.spark.sql.{Column, DataFrame}
 
 import scala.reflect.ClassTag
 
@@ -13,12 +13,6 @@ import scala.reflect.ClassTag
   */
 package object dsl {
 
-  // Type Aliases
-  type DFFunc = (DataFrame) => DataFrame
-  type DFFunc2 = (SQLContext) => DataFrame
-  type ColFunc = (Column) => Column
-  type DFJFunc = (DataFrame, DataFrame, String) => DataFrame
-  type DFGFunc = (DataFrame, Seq[Column]) => DataFrame
 
   /*
      No Operation Transform
@@ -26,6 +20,8 @@ package object dsl {
   object NoOp {
     def apply(): DFFunc = (df: DataFrame) => df
   }
+
+
 
   /*
      Select Transform
@@ -40,6 +36,9 @@ package object dsl {
     def apply(joinedDF: DataFrame, alias: String): DFFunc = (df: DataFrame) => df.select(joinedDF.getColumns(alias): _*)
   }
 
+
+
+
   /*
     Filter Select Transform
   */
@@ -50,6 +49,9 @@ package object dsl {
       (df: DataFrame) => df.select(joinedDF.getColumns(alias, excludeColumn): _*)
   }
 
+
+
+
   /*
     Filter Transform
   */
@@ -58,6 +60,9 @@ package object dsl {
 
     def apply[_: ClassTag](filterCondition: Column): DFFunc = (df: DataFrame) => df.filter(filterCondition)
   }
+
+
+
 
   /*
       Rename Transform
@@ -73,12 +78,18 @@ package object dsl {
           df.renameColumns(df.columns.map(col => col -> s"$col$append").toMap)
   }
 
+
+
+
   /*
     Drop Transform
   */
   object Drop {
     def apply(dropColumns: String*): DFFunc = (df: DataFrame) => df.dropColumns(dropColumns)
   }
+
+
+
 
   /*
     Column Transformations
@@ -95,6 +106,9 @@ package object dsl {
       df.applyColumnExpressions(df.columns.map(column => column -> columnFunc(col(column))).toMap)
   }
 
+
+
+
   /*
       Generate Sequence numbers for a column based on the previous Maximum sequence value
    */
@@ -104,42 +118,54 @@ package object dsl {
     def apply(startIndex: Long, column: String): DFFunc = (df: DataFrame) => df.generateSequence(startIndex, Some(column))
   }
 
+
+
+
   /*
     Join Transform
    */
   object Join {
-    def apply(conditions: String*): DFJFunc =
+    def apply(conditions: String*): DFJoinFunc =
       (ldf: DataFrame, rdf: DataFrame, jType: String) => ldf.join(rdf, conditions.toSeq, jType)
 
-    def apply(conditions: Column): DFJFunc =
+    def apply(conditions: Column): DFJoinFunc =
       (ldf: DataFrame, rdf: DataFrame, jType: String) => ldf.join(rdf, conditions, jType)
   }
+
+
+
 
   /*
     Group Transform
    */
   object Group {
-    def apply(groupColumns: String*): DFGFunc =
+    def apply(groupColumns: String*): DFGroupFunc =
       (df: DataFrame, columns: Seq[Column]) =>
         df.groupBy(groupColumns.head, groupColumns.tail: _*).agg(columns.head, columns.tail: _*)
 
-    def apply[_: ClassTag](groupColumns: Column*): DFGFunc =
+    def apply[_: ClassTag](groupColumns: Column*): DFGroupFunc =
       (df: DataFrame, columns: Seq[Column]) =>
         df.groupBy(groupColumns: _*).agg(columns.head, columns.tail: _*)
   }
+
+
+
 
   /*
     Cube Transform
    */
   object Cube {
-    def apply(groupColumns: String*): DFGFunc =
+    def apply(groupColumns: String*): DFGroupFunc =
       (df: DataFrame, columns: Seq[Column]) =>
         df.cube(groupColumns.head, groupColumns.tail: _*).agg(columns.head, columns.tail: _*)
 
-    def apply[_: ClassTag](groupColumns: Column*): DFGFunc =
+    def apply[_: ClassTag](groupColumns: Column*): DFGroupFunc =
       (df: DataFrame, columns: Seq[Column]) =>
         df.cube(groupColumns: _*).agg(columns.head, columns.tail: _*)
   }
+
+
+
 
   /*
     Unstruct the fields in a given Struct as columns.
@@ -148,6 +174,9 @@ package object dsl {
     def apply(unstructCol: String, keepStructColumn: Boolean = true): DFFunc =
       (df: DataFrame) => df.unstruct(unstructCol, keepStructColumn)
   }
+
+
+
 
   /*
       Update/Add Surrogate key columns by joining on the 'Key' dimension table.
@@ -158,27 +187,50 @@ package object dsl {
   }
 
 
+
+
   implicit class DFImplicits(dataframe: DataFrame) {
     def -->(func: DFFunc): DataFrame = dataframe.transform(func)
   }
 
+
+
+
   implicit class DFFuncImplicits(dfFunc: DFFunc) {
     def -->(df: DataFrame): DataFrame = df.transform(dfFunc)
+
+    def using(df: DataFrame): DataFrame = df.transform(dfFunc)
 
     def +(rightDFFunc: DFFunc): DFFunc = dfFunc andThen rightDFFunc
   }
 
-  implicit class DFJFuncImplicits(dfJFunc: DFJFunc) {
+
+
+
+
+  implicit class DFJoinFuncImplicits(dfJFunc: DFJoinFunc) {
+
+    def inner(rightDF: DataFrame): DFFunc = dfJFunc(_: DataFrame, rightDF, "inner")
+
     def ><(rightDF: DataFrame): DFFunc = dfJFunc(_: DataFrame, rightDF, "inner")
+
+    def left(rightDF: DataFrame): DFFunc = dfJFunc(_: DataFrame, rightDF, "left")
 
     def <<(rightDF: DataFrame): DFFunc = dfJFunc(_: DataFrame, rightDF, "left")
 
+    def right(rightDF: DataFrame): DFFunc = dfJFunc(_: DataFrame, rightDF, "right")
+
     def >>(rightDF: DataFrame): DFFunc = dfJFunc(_: DataFrame, rightDF, "right")
+
+    def full(rightDF: DataFrame): DFFunc = dfJFunc(_: DataFrame, rightDF, "full")
 
     def <>(rightDF: DataFrame): DFFunc = dfJFunc(_: DataFrame, rightDF, "full")
   }
 
-  implicit class DFGFuncImplicits(dfGFunc: DFGFunc) {
+
+
+
+  implicit class DFGroupFuncImplicits(dfGFunc: DFGroupFunc) {
     def ^(joinExprs: Column*): DFFunc = dfGFunc(_: DataFrame, joinExprs)
 
     def ^[_: ClassTag](joinExprs: String*): DFFunc = dfGFunc(_: DataFrame, joinExprs.map(expr))
