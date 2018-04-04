9412e5361c646fd05177386f72fbfea25f097f42
mayur2810 <mayur.mhi@gmail.com>
2018-04-04 19:48:59 +0530

    updated yaml runner

--- dwp-utils/src/main/scala/com/mayurb/dwp/transform/YamlRunner.scala
+++ dwp-utils/src/main/scala/com/mayurb/dwp/transform/YamlRunner.scala
@@ -1,8 +1,9 @@
 package com.mayurb.dwp.transform
 
 import com.mayurb.utils.Logging
-import org.apache.spark.sql.hive.HiveContext
-import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.commons.cli.{BasicParser, Options}
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.SparkSession
 
 /**
   * Helper for Running YAML Transformation
@@ -11,17 +12,21 @@ import org.apache.spark.{SparkConf, SparkContext}
   */
 object YamlRunner extends Logging {
 
+  private val YamlPathOpt = "yaml_file"
+  private val options = new Options().addOption(YamlPathOpt, true, "yaml file path")
+
   def main(args: Array[String]): Unit = {
-    if (args.length == 0) {
-      throw new Exception("Yaml file path required")
-    }
-    val yamlPath = args(0)
+    val cmdLine = new BasicParser().parse(options, args, true)
+    val optionMap = cmdLine.getOptions.map(option => option.getOpt.trim -> option.getValue.trim).toMap
+    val yamlPath =  optionMap(YamlPathOpt)
     val yamlTransformer = new YamlDataTransform(yamlPath)
     logInfo("Successfully parsed YAML File, executing the Flow")
-    val sparkConf = new SparkConf().setAppName("YAML Transformer")
-    val sparkContext = new SparkContext(sparkConf)
-    val sqlContext = new HiveContext(sparkContext)
-    yamlTransformer.performTransformations(sqlContext)
+    val sparkConf = new SparkConf().setAppName("Spark: YAML Transformer")
+    val session = SparkSession.builder()
+      .config(sparkConf)
+      .enableHiveSupport()
+      .getOrCreate()
+    yamlTransformer.performTransformations(session.sqlContext)
   }
 
 }
