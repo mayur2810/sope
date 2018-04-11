2be70f2c1b61bf4f687158de21d80d4ad20ad56f
mayur2810 <mayur.mhi@gmail.com>
2018-04-11 19:20:48 +0530

    - added DimensionTable.scala which support SCD process for dimension table - added unit test cases for scd scenarios

--- dwp-utils/src/test/scala/com/mayurb/dwp/model/package.scala
+++ dwp-utils/src/test/scala/com/mayurb/dwp/model/package.scala
@@ -9,7 +9,10 @@ package object model {
 
   case class Transactions(id: Int, loc: String, product: String, date: String)
 
-  case class Product(product_id: Int, product_desc: String, size: Int, color: Int)
+  case class Product(product_id: Int, product_desc: String, size: Int, color: Int, derived_attr: String = "")
+
+  case class ProductDim(product_key: Long, product_id: Int, product_desc: String, size: Int, color: Int, derived_attr: String,
+                        last_updated_date: java.sql.Date)
 
   case class Date(date_id: Int, date: String)
 
