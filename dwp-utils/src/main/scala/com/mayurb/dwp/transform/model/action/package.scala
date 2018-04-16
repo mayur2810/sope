6dc61dd36e86cdccfdd60a8054e5efc50e67a368
mayur2810 <mayur.mhi@gmail.com>
2018-04-16 13:25:11 +0530

    - Added new actions: order_by, limit, distinct, union, intersect, drop-duplicates - Refactored code

--- dwp-utils/src/main/scala/com/mayurb/dwp/transform/model/action/package.scala
+++ dwp-utils/src/main/scala/com/mayurb/dwp/transform/model/action/package.scala
@@ -15,33 +15,71 @@ import org.apache.spark.sql.functions.expr
   */
 package object action {
 
+  /*
+     ETL Actions
+   */
+  object Actions {
+    final val Rename = "rename"
+    final val Filter = "filter"
+    final val Join = "join"
+    final val GroupBy = "group_by"
+    final val Transform = "transform"
+    final val Select = "select"
+    final val SelectNot = "select_not"
+    final val Distinct = "distinct"
+    final val Limit = "limit"
+    final val Union = "union"
+    final val Intersect = "intersect"
+    final val Except = "except"
+    final val Sequence = "sequence"
+    final val SCD = "scd"
+    final val OrderBy = "order_by"
+    final val DropDuplicates = "drop_duplicates"
+    final val DropColumn = "drop"
+    final val Unstruct = "unstruct"
+  }
+
+  /**
+    * Root Class for Transform Action. To be extended by each Actions.
+    *
+    * @param id Action Id
+    */
   @JsonTypeInfo(
     use = JsonTypeInfo.Id.NAME,
     include = JsonTypeInfo.As.PROPERTY,
     property = "type")
   @JsonSubTypes(Array(
-    new Type(value = classOf[RenameAction], name = "rename"),
-    new Type(value = classOf[FilterAction], name = "filter"),
-    new Type(value = classOf[JoinAction], name = "join"),
-    new Type(value = classOf[GroupAction], name = "group"),
-    new Type(value = classOf[TransformAction], name = "transform"),
-    new Type(value = classOf[RenameAction], name = "rename"),
-    new Type(value = classOf[SelectAction], name = "select"),
-    new Type(value = classOf[SelectNotAction], name = "select_not"),
-    new Type(value = classOf[SequenceAction], name = "sequence"),
-    new Type(value = classOf[SCDAction], name = "scd")
+    new Type(value = classOf[RenameAction], name = Actions.Rename),
+    new Type(value = classOf[FilterAction], name = Actions.Filter),
+    new Type(value = classOf[JoinAction], name = Actions.Join),
+    new Type(value = classOf[GroupAction], name = Actions.GroupBy),
+    new Type(value = classOf[TransformAction], name = Actions.Transform),
+    new Type(value = classOf[SelectAction], name = Actions.Select),
+    new Type(value = classOf[SelectNotAction], name = Actions.SelectNot),
+    new Type(value = classOf[DistinctAction], name = Actions.Distinct),
+    new Type(value = classOf[LimitAction], name = Actions.Limit),
+    new Type(value = classOf[UnionAction], name = Actions.Union),
+    new Type(value = classOf[IntersectAction], name = Actions.Intersect),
+    new Type(value = classOf[SequenceAction], name = Actions.Sequence),
+    new Type(value = classOf[SCDAction], name = Actions.SCD),
+    new Type(value = classOf[OrderByAction], name = Actions.OrderBy),
+    new Type(value = classOf[DropDuplicateAction], name = Actions.DropDuplicates),
+    new Type(value = classOf[DropColumnAction], name = Actions.DropColumn),
+    new Type(value = classOf[UnstructAction], name = Actions.Unstruct)
   ))
   abstract class TransformActionRoot(@JsonProperty(value = "type", required = true) id: String) {
     def apply(dataframes: DataFrame*): DFFunc
+
+    def inputAliases: Seq[String] = Nil
   }
 
 
-  case class RenameAction(@JsonProperty(required = true) list: Map[String, String]) extends TransformActionRoot("rename") {
+  case class RenameAction(@JsonProperty(required = true) list: Map[String, String]) extends TransformActionRoot(Actions.Rename) {
     override def apply(dataframes: DataFrame*): DFFunc = Rename(list.toSeq: _*)
   }
 
 
-  case class TransformAction(@JsonProperty(required = true) list: Map[String, String]) extends TransformActionRoot("transform") {
+  case class TransformAction(@JsonProperty(required = true) list: Map[String, String]) extends TransformActionRoot(Actions.Transform) {
     override def apply(dataframes: DataFrame*): DFFunc = Transform(list.toSeq: _*)
   }
 
@@ -50,7 +88,7 @@ package object action {
                         @JsonProperty(value = "columns", required = false) joinColumns: Seq[String],
                         @JsonProperty(value = "join_type", required = true) joinType: String,
                         @JsonProperty(value = "with", required = true) joinSource: String,
-                        @JsonProperty(value = "broadcast_hint") broadcastHint: String) extends TransformActionRoot("join") {
+                        @JsonProperty(value = "broadcast_hint") broadcastHint: String) extends TransformActionRoot(Actions.Join) {
 
     private val joinTypeFunc: (DFJoinFunc) => DataFrame => DFFunc = (joinFunc: DFJoinFunc) => joinType match {
       case "inner" => joinFunc >< _
@@ -60,37 +98,97 @@ package object action {
     }
 
     override def apply(dataframes: DataFrame*): DFFunc = {
-      if (joinCondition == null && joinColumns == null) throw new Exception("Please provide either join 'condition' or join 'columns' option")
+      if (joinCondition == null && joinColumns == null)
+        throw new Exception("Please provide either join 'condition' or join 'columns' option")
+
       if (joinCondition != null)
         joinTypeFunc(Join(Option(broadcastHint), expr(joinCondition)))(dataframes.head)
       else
         joinTypeFunc(Join(Option(broadcastHint), joinColumns: _*))(dataframes.head)
     }
+
+    override def inputAliases: Seq[String] = Seq(joinSource)
   }
 
 
   case class GroupAction(@JsonProperty(value = "columns", required = true) groupColumns: Seq[String],
-                         @JsonProperty(value = "expr", required = true) groupExpr: String) extends TransformActionRoot("group") {
+                         @JsonProperty(value = "expr", required = true) groupExpr: String)
+    extends TransformActionRoot(Actions.GroupBy) {
     override def apply(dataframes: DataFrame*): DFFunc = Group(groupColumns: _*) ^ groupExpr
   }
 
 
-  case class FilterAction(@JsonProperty(required = true) condition: String) extends TransformActionRoot("filter") {
+  case class FilterAction(@JsonProperty(required = true) condition: String)
+    extends TransformActionRoot(Actions.Filter) {
     override def apply(dataframes: DataFrame*): DFFunc = Filter(condition)
   }
 
 
-  case class SelectAction(@JsonProperty(required = true) columns: Seq[String]) extends TransformActionRoot("select") {
+  case class SelectAction(@JsonProperty(required = true) columns: Seq[String])
+    extends TransformActionRoot(Actions.Select) {
     override def apply(dataframes: DataFrame*): DFFunc = Select(columns: _*)
   }
 
-  case class SelectNotAction(@JsonProperty(required = true) columns: Seq[String]) extends TransformActionRoot("select_not") {
+  case class SelectNotAction(@JsonProperty(required = true) columns: Seq[String])
+    extends TransformActionRoot(Actions.SelectNot) {
     override def apply(dataframes: DataFrame*): DFFunc = SelectNot(columns: _*)
   }
 
+  case class UnionAction(@JsonProperty(required = true, value = "with") unionWith: Seq[String])
+    extends TransformActionRoot(Actions.Union) {
+    override def apply(dataframes: DataFrame*): DFFunc = Union(dataframes: _*)
+
+    override def inputAliases: Seq[String] = unionWith
+  }
+
+  case class IntersectAction(@JsonProperty(required = true, value = "with") intersectWith: Seq[String])
+    extends TransformActionRoot(Actions.Intersect) {
+    override def apply(dataframes: DataFrame*): DFFunc = Intersect(dataframes: _*)
+
+    override def inputAliases: Seq[String] = intersectWith
+  }
+
+  case class ExceptAction(@JsonProperty(required = true, value = "with") exceptWith: Seq[String])
+    extends TransformActionRoot(Actions.Except) {
+    override def apply(dataframes: DataFrame*): DFFunc = Except(dataframes: _*)
+
+    override def inputAliases: Seq[String] = exceptWith
+  }
+
+  case class OrderByAction(@JsonProperty(required = true) columns: Seq[String])
+    extends TransformActionRoot(Actions.OrderBy) {
+    override def apply(dataframes: DataFrame*): DFFunc = OrderBy(columns: _*)
+  }
+
+  case class LimitAction(@JsonProperty(required = true) size: Int) extends TransformActionRoot(Actions.Limit) {
+    override def apply(dataframes: DataFrame*): DFFunc = Limit(size)
+  }
+
+  case class DistinctAction() extends TransformActionRoot(Actions.Distinct) {
+    override def apply(dataframes: DataFrame*): DFFunc = Distinct.apply
+  }
+
+  case class DropDuplicateAction(@JsonProperty(required = true) columns: Seq[String])
+    extends TransformActionRoot(Actions.DropDuplicates) {
+    override def apply(dataframes: DataFrame*): DFFunc = DropDuplicates(columns: _*)
+  }
+
+  case class DropColumnAction(@JsonProperty(required = true) columns: Seq[String])
+    extends TransformActionRoot(Actions.DropColumn) {
+    override def apply(dataframes: DataFrame*): DFFunc = Drop(columns: _*)
+  }
+
+  case class UnstructAction(@JsonProperty(required = true) column: String)
+    extends TransformActionRoot(Actions.Unstruct) {
+    override def apply(dataframes: DataFrame*): DFFunc = Unstruct(column)
+  }
+
   case class SequenceAction(@JsonProperty(value = "sk_source", required = true) skSource: String,
-                            @JsonProperty(value = "sk_column", required = true) skColumn: String) extends TransformActionRoot("sequence") {
+                            @JsonProperty(value = "sk_column", required = true) skColumn: String)
+    extends TransformActionRoot(Actions.Sequence) {
     override def apply(dataframes: DataFrame*): DFFunc = Sequence(dataframes.head.maxKeyValue(skColumn), skColumn)
+
+    override def inputAliases: Seq[String] = Seq(skSource)
   }
 
   case class SCDAction(@JsonProperty(value = "dim_table", required = true) dimTable: String,
@@ -99,10 +197,12 @@ package object action {
                        @JsonProperty(value = "derived_columns", required = true) derivedColumns: Seq[String],
                        @JsonProperty(value = "meta_columns", required = true) metaColumns: Seq[String],
                        @JsonProperty(value = "incremental_load", required = false) incrementalLoad: Option[Boolean])
-    extends TransformActionRoot("scd") {
+    extends TransformActionRoot(Actions.SCD) {
     override def apply(dataframes: DataFrame*): DFFunc = (scdInput: DataFrame) =>
       new DimensionTable(dataframes.head, surrogateKey, naturalKeys, derivedColumns, metaColumns)
         .getDimensionChangeSet(scdInput, incrementalLoad.getOrElse(true)).getUnion
+
+    override def inputAliases: Seq[String] = Seq(dimTable)
   }
 
 }
