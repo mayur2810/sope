766f76d9982d2562bfc8904be8e3c03f1ff67285
mayur2810 <mayur.mhi@gmail.com>
2019-07-01 16:51:56 +0530

    Fixed quoting issue during substitution. Moved the control to user to handle escaping in the yaml

--- sope-etl/src/main/scala/com/sope/etl/yaml/YamlFile.scala
+++ sope-etl/src/main/scala/com/sope/etl/yaml/YamlFile.scala
@@ -21,8 +21,8 @@ abstract class YamlFile[T](yamlPath: String, substitutions: Option[Map[String, A
    */
   private def updatePlaceHolders(): String = {
     substitutions.get
-      .map { case (placeholder, substitution) => "\"*\\s*\\$\\{" + placeholder.trim + "\\}\\s*\"*" -> convertToYaml(substitution).trim }
-      .foldLeft(readYamlFile(yamlPath)) { case (yamlStr, (key, value)) => yamlStr.replaceAll(key, s" $value")
+      .map { case (placeholder, substitution) => "\\$\\{" + placeholder.trim + "\\}" -> convertToYaml(substitution).trim }
+      .foldLeft(readYamlFile(yamlPath)) { case (yamlStr, (key, value)) => yamlStr.replaceAll(key, value)
       }
   }
 
