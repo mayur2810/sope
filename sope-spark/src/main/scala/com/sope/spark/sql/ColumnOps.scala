0d119af0f2ab5ee35071f2536163b3b3faff8a2b
mayur2810 <mayur.mhi@gmail.com>
2020-08-07 19:59:08 +0200

    added multiArgToSingleArgFunc

--- sope-spark/src/main/scala/com/sope/spark/sql/ColumnOps.scala
+++ sope-spark/src/main/scala/com/sope/spark/sql/ColumnOps.scala
@@ -14,4 +14,16 @@ object ColumnOps extends SqlColumnOps[Column] {
   override def resolveMultiArgFunction(functionName: String): Types.MultiColFunc[Column] =
     (columns: Seq[Column]) => callUDF(functionName, columns: _*)
 
+
+  /**
+   * Applies know arguments to MultiArg Function in order provided and returns
+   * a single arg function to which the actual column can be provided
+   *
+   * @param multiArgFunc Multi Argument Column Function
+   * @param columns      Columns to be applied
+   * @return [[ColFunc]]
+   */
+  def multiArgToSingleArgFunc(multiArgFunc: Types.MultiColFunc[Column], columns: Seq[Column]): ColFunc =
+    (column: Column) => multiArgFunc(column +: columns)
+
 }
